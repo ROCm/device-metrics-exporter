bhatturu/gpu-agent

https://pensando.atlassian.net/browse/GPUOP-350 (#688)
GPUOP-350: Exporter to be made configurable health monitor services

Fix : added configmap support for disabling health monitoring grpc
server hosted by metrics exporter

(cherry picked from commit 6334b2d1656eeb6472046d816a8e583a42aefb37)

pkg/exporter/exporter.go
@@ -65,6 +65,8 @@ type Exporter struct {
 	enableGPUMonitoring bool
 	enableSriov         bool
 	k8sApiClient        *k8sclient.K8sClient
+	svcHandler          *metricsserver.SvcHandler
+	k8sScl              scheduler.SchedulerClient
 	ctx                 context.Context
 	cancel              context.CancelFunc
 }
@@ -125,7 +127,7 @@ func startMetricsServer(c *config.ConfigHandler) *http.Server {
 	return srv
 }
 
-func foreverWatcher(ctx context.Context) {
+func foreverWatcher(e *Exporter) {
 	var srvHandler *http.Server
 	configPath := runConf.GetMetricsConfigPath()
 	directory := path.Dir(configPath)
@@ -144,6 +146,7 @@ func foreverWatcher(ctx context.Context) {
 			serverPort := runConf.GetServerPort()
 			logger.Log.Printf("starting server on %v", serverPort)
 			srvHandler = startMetricsServer(runConf)
+			go e.svcHandler.Run()
 
 		}
 	}
@@ -157,6 +160,7 @@ func foreverWatcher(ctx context.Context) {
 			srvCancel()
 			time.Sleep(1 * time.Second)
 			srvHandler = nil
+			e.svcHandler.Stop()
 		}
 	}
 
@@ -178,7 +182,7 @@ func foreverWatcher(ctx context.Context) {
 		}
 		debounce.Reset(debounceDuration)
 
-		for ctx.Err() == nil {
+		for e.ctx.Err() == nil {
 			select {
 			case event, ok := <-watcher.Events:
 				if !ok {
@@ -214,7 +218,7 @@ func foreverWatcher(ctx context.Context) {
 
 	logger.Log.Printf("starting file watcher for %v", configPath)
 
-	<-ctx.Done()
+	<-e.ctx.Done()
 	stopServer()
 	logger.Log.Printf("file watcher stopped due to context cancellation")
 }
@@ -308,32 +312,24 @@ func (e *Exporter) StartMain(enableDebugAPI bool) {
 	defer e.Close()
 	logger.Init(utils.IsKubernetes())
 
-	svcHandler := metricsserver.InitSvcs(
-		metricsserver.WithDebugAPIOption(enableDebugAPI),
-		metricsserver.WithNICMonitoring(e.enableNICMonitoring),
-		metricsserver.WithGPUMonitoring(e.enableGPUMonitoring),
-	)
-	go func() {
-		logger.Log.Printf("metrics service starting")
-		if err := svcHandler.Run(); err != nil {
-			logger.Log.Printf("metrics service start failed: %+v", err)
-		}
-		logger.Log.Printf("metrics service stopped")
-		os.Exit(0)
-	}()
-
 	runConf = config.NewConfigHandler(e.configFile, e.agentGrpcPort)
 
 	mh, _ = metricsutil.NewMetrics(runConf)
 	mh.InitConfig()
 
+	e.svcHandler = metricsserver.InitSvcs(mh,
+		metricsserver.WithDebugAPIOption(enableDebugAPI),
+		metricsserver.WithNICMonitoring(e.enableNICMonitoring),
+		metricsserver.WithGPUMonitoring(e.enableGPUMonitoring),
+	)
+
 	// create scheduler client
-	var k8sScl scheduler.SchedulerClient
-	var err error
 	if utils.IsKubernetes() {
-		k8sScl, err = scheduler.NewKubernetesClient(e.ctx)
+		k8sScl, err := scheduler.NewKubernetesClient(e.ctx)
 		if err != nil {
 			logger.Log.Printf("failed to create k8s scheduler client: %v", err)
+		} else {
+			e.k8sScl = k8sScl
 		}
 	}
 
@@ -342,25 +338,25 @@ func (e *Exporter) StartMain(enableDebugAPI bool) {
 			gpuagent.WithZmq(!e.zmqDisable),
 			gpuagent.WithK8sClient(e.GetK8sApiClient()),
 			gpuagent.WithSRIOV(e.enableSriov),
-			gpuagent.WithK8sSchedulerClient(k8sScl),
+			gpuagent.WithK8sSchedulerClient(e.k8sScl),
 		)
 		if err := gpuclient.Init(); err != nil {
 			logger.Log.Printf("gpuclient init err :%+v", err)
 		}
 		e.startWatchers()
-		if err := svcHandler.RegisterGPUHealthClient(gpuclient); err != nil {
+		if err := e.svcHandler.RegisterGPUHealthClient(gpuclient); err != nil {
 			logger.Log.Printf("health client registration err: %+v", err)
 		}
 	}
 
 	if e.enableNICMonitoring {
 		nicAgent = nicagent.NewAgent(mh,
-			nicagent.WithK8sSchedulerClient(k8sScl),
+			nicagent.WithK8sSchedulerClient(e.k8sScl),
 		)
 		if err := nicAgent.Init(); err != nil {
 			logger.Log.Printf("nic client init err :%+v", err)
 		}
-		if err := svcHandler.RegisterNICHealthClient(nicAgent); err != nil {
+		if err := e.svcHandler.RegisterNICHealthClient(nicAgent); err != nil {
 			logger.Log.Printf("nic health client registration err: %+v", err)
 		}
 		nicHealth, err := nicAgent.GetNICHealthStates()
@@ -371,7 +367,7 @@ func (e *Exporter) StartMain(enableDebugAPI bool) {
 		copyFilesToHost()
 	}
 	// start file watcher for config changes
-	foreverWatcher(e.ctx)
+	foreverWatcher(e)
 }
 
 // SetComputeNodeHealth sets the compute node health

ROCm/device-metrics-exporter

pkg/exporter/exporter.go
@@ -62,6 +62,7 @@ type Exporter struct {
 	configFile    string
 	zmqDisable    bool
 	k8sApiClient  *k8sclient.K8sClient
+	svcHandler    *metricsserver.SvcHandler
 	ctx           context.Context
 	cancel        context.CancelFunc
 }
@@ -120,7 +121,7 @@ func startMetricsServer(c *config.ConfigHandler) *http.Server {
 	return srv
 }
 
-func foreverWatcher(ctx context.Context) {
+func foreverWatcher(e *Exporter) {
 	var srvHandler *http.Server
 	configPath := runConf.GetMetricsConfigPath()
 	directory := path.Dir(configPath)
@@ -139,6 +140,7 @@ func foreverWatcher(ctx context.Context) {
 			serverPort := runConf.GetServerPort()
 			logger.Log.Printf("starting server on %v", serverPort)
 			srvHandler = startMetricsServer(runConf)
+			go e.svcHandler.Run()
 
 		}
 	}
@@ -152,6 +154,7 @@ func foreverWatcher(ctx context.Context) {
 			srvCancel()
 			time.Sleep(1 * time.Second)
 			srvHandler = nil
+			e.svcHandler.Stop()
 		}
 	}
 
@@ -173,7 +176,7 @@ func foreverWatcher(ctx context.Context) {
 		}
 		debounce.Reset(debounceDuration)
 
-		for ctx.Err() == nil {
+		for e.ctx.Err() == nil {
 			select {
 			case event, ok := <-watcher.Events:
 				if !ok {
@@ -209,7 +212,7 @@ func foreverWatcher(ctx context.Context) {
 
 	logger.Log.Printf("starting file watcher for %v", configPath)
 
-	<-ctx.Done()
+	<-e.ctx.Done()
 	stopServer()
 	logger.Log.Printf("file watcher stopped due to context cancellation")
 }
@@ -282,20 +285,12 @@ func (e *Exporter) StartMain(enableDebugAPI bool) {
 
 	logger.Init(utils.IsKubernetes())
 
-	svcHandler := metricsserver.InitSvcs(enableDebugAPI)
-	go func() {
-		logger.Log.Printf("metrics service starting")
-		if err := svcHandler.Run(); err != nil {
-			logger.Log.Printf("metrics service start failed: %+v", err)
-		}
-		logger.Log.Printf("metrics service stopped")
-		os.Exit(0)
-	}()
-
 	runConf = config.NewConfigHandler(e.configFile, e.agentGrpcPort)
 
 	mh, _ = metricsutil.NewMetrics(runConf)
 	mh.InitConfig()
+
+	e.svcHandler = metricsserver.InitSvcs(enableDebugAPI, mh)
 
 	gpuclient = gpuagent.NewAgent(mh, e.GetK8sApiClient(), !e.zmqDisable)
 	if err := gpuclient.Init(); err != nil {
@@ -305,11 +300,11 @@ func (e *Exporter) StartMain(enableDebugAPI bool) {
 
 	e.startWatchers()
 
-	if err := svcHandler.RegisterHealthClient(gpuclient); err != nil {
+	if err := e.svcHandler.RegisterHealthClient(gpuclient); err != nil {
 		logger.Log.Printf("health client registration err: %+v", err)
 	}
 
-	foreverWatcher(e.ctx)
+	foreverWatcher(e)
 }
 
 // Close - closes the exporter and all its resources